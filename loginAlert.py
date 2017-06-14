b33b36fcdcc45fd43000278004c6d50c16abaeca
zhanglintc <zhanglintc623@gmail.com>
2017-06-14 14:00:23 +0800

    use threading

--- loginAlert.py
+++ loginAlert.py
@@ -2,18 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import sendMsg
+import threading
 import os
 
-stdout = os.popen('whoami')
-whoami = stdout.read().strip()
+def login_message():
+    stdout = os.popen('whoami')
+    whoami = stdout.read().strip()
 
-stdout = os.popen('date "+%H:%M:%S %m-%d"')
-date = stdout.read().strip()
+    stdout = os.popen('date "+%H:%M:%S %m-%d"')
+    date = stdout.read().strip()
 
-msg = ""
-msg += "Login Alert\n"
-msg += "User: {0}\n".format(whoami)
-msg += "Time: {0}\n".format(date)
+    msg = ""
+    msg += "Login Alert\n"
+    msg += "User: {0}\n".format(whoami)
+    msg += "Time: {0}\n".format(date)
 
-sendMsg.sendMsg(content=msg, touser="zhanglintc|smile")
+    sendMsg.sendMsg(content=msg, touser="zhanglintc|smile")
+
+if __name__ == '__main__':
+    threading.Thread(target=login_message).start()
+
 
