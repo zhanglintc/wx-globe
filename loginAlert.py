f149953c896aff8525d27bbfa65ea0454efd3d56
zhanglintc <zhanglintc623@gmail.com>
2018-07-30 09:09:36 +0800

    notify only once within one hour

--- loginAlert.py
+++ loginAlert.py
@@ -3,7 +3,9 @@
 
 import sendMsg
 import threading
-import os
+import os, json, time
+
+time_log_file = "login-time.json"
 
 def login_message():
     stdout = os.popen('whoami')
@@ -17,7 +19,27 @@ def login_message():
     msg += "User: {0}\n".format(whoami)
     msg += "Time: {0}\n".format(date)
 
-    sendMsg.sendMsg(content=msg, touser="zhanglintc|smile")
+    try:
+        fr = open(time_log_file, "rb")
+        content = fr.read()
+        jsn = json.loads(content)
+        fr.close()
+    except:
+        jsn = {}
+
+    log_time = jsn.get(whoami, 0)
+    cur_time = time.time()
+
+    if cur_time > log_time + 3600:
+        sendMsg.sendMsg(content=msg, touser="zhanglintc|smile")
+
+        fw = open(time_log_file, "wb")
+        jsn[whoami] = cur_time
+        content = json.dumps(jsn, indent=4)
+        fw.write(content)
+        fw.close()
+    else:
+        print("last notification within 1 hour")
 
 if __name__ == '__main__':
     threading.Thread(target=login_message).start()
